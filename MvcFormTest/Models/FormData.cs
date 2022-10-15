68a184b879e2a6d6e839053fb31d89d24de19411
David Buckell <davidbuckell1978@gmail.com>
2022-10-15 17:07:02 +0100

    update model strings to nullables to avoid null values being considered invalid

--- MvcFormTest/Models/FormData.cs
+++ MvcFormTest/Models/FormData.cs
@@ -4,9 +4,9 @@ namespace MvcFormTest.Models;
 
 public class FormData
 {
-    public string Title { get; set; }
+    public string? Title { get; set; }
     [Required]
-    public string Name { get; set; }
+    public string? Name { get; set; }
     [Range(18, 21)]
     public int Age { get; set; }
 }
